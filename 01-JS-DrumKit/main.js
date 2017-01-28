e390f92e628411a677a11d79b11a1a7bd254add2
quangnd <quangnd.edu@gmail.com>
2017-01-28 09:09:17 +0700

    Update readme

--- 01-JS-DrumKit/main.js
+++ 01-JS-DrumKit/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(e){
     const keys = Array.from(document.querySelectorAll('.key'));
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
     window.addEventListener('keydown', playSound);
+    window.addEventListener('click', clickToPlaySound);
 });
 
 
@@ -11,11 +12,24 @@ function removeTransition(e) {
 }
 
 function playSound(e) {
+
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing'); //add CSS class
+    audio.currentTime = 0; //sets the current position of the audio playback.
+    audio.play();
+}
+
+function clickToPlaySound(e) {
+    const textContent = e.target.textContent;
+    const audioKeyCode = textContent.charCodeAt(0);
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`);
+     const key = document.querySelector(`div[data-key="${audioKeyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing'); 
     audio.currentTime = 0; //sets the current position of the audio playback.
     audio.play();
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-Readme
+Goal after lesson
+--
+
+1. Note about the new knowledge
+2. Note about the difficult knowledge
+3. Some ideas can expand based on lesson
 
